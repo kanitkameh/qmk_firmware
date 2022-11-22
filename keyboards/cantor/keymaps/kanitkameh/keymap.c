6c46676701fa12fd0688369ac8cdb515efd031a5
unknown <kanitkameh@gmail.com>
2022-11-22 23:43:58 +0200

    add CAPS_WORD

--- keyboards/cantor/keymaps/kanitkameh/keymap.c
+++ keyboards/cantor/keymaps/kanitkameh/keymap.c
@@ -30,19 +30,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───┘   └───┘
       */
     [BASE] = LAYOUT_split_3x6_3(
-        KC_CAPS,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LCBR,
-        KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_GRAVE, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCBR,
+        CAPS_WORD, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LCBR,
+        KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_GRAVE,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCBR,
                             SH_TG, LT(LEFT_MODS, KC_SPC), LT(NUMBERS, KC_TAB),   LT(NUMBERS, KC_BSPC), LT(RIGHT_MODS, KC_ENT), LT(VIM, KC_DEL)
     ),
     [GAMING] = LAYOUT_split_3x6_3(
-        _______, _______,  _______, _______, _______, _______,                                _______, _______, _______, _______, _______, _______,
+        KC_CAPS, _______,  _______, _______, _______, _______,                                _______, _______, _______, _______, _______, _______,
         _______, _______,  _______, _______, _______, _______,                                _______, _______, _______, _______, _______, _______,
         _______, _______,  _______, _______, _______, _______,                                _______, _______, _______, _______, _______, _______,
                                            _______, KC_SPC, _______,                     _______, _______, _______
     ),
     [GAMING_SHIFTED] = LAYOUT_split_3x6_3(
-        _______,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,                                   _______, _______, _______, _______, _______, _______,
+        KC_CAPS,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,                                   _______, _______, _______, _______, _______, _______,
         _______,  KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,                                   _______, _______, _______, _______, _______, _______,
         _______,  KC_LCTRL,  KC_Z,    KC_X,    KC_C,    KC_V,                                   _______, _______, _______, _______, _______, _______,
                                 _______, KC_SPC, LT(NUMBERS, KC_LALT),                      _______, _______, _______
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NUMBERS] = LAYOUT_split_3x6_3(
         KC_F12,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                               KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         KC_EQL,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS,
-        _______, _______,  _______,  _______,  _______,  _______,                             _______, _______, _______, _______, KC_BSLS, _______,
+        _______, _______,  _______,  _______,  _______,  _______,                             _______, _______, _______, _______, _______, KC_BSLS,
                                                  _______, _______, _______,          _______, _______, _______
     ),
     [LEFT_MODS] = LAYOUT_split_3x6_3(
