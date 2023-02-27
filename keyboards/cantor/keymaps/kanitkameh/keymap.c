fe9fb26323510bd5349f0d36ba8f7a060355943f
Kamen Vakavchiev <kanitkameh@debian-BULLSEYE-live-builder-AMD64>
2023-02-27 17:02:27 +0200

    more convenient braces

--- keyboards/cantor/keymaps/kanitkameh/keymap.c
+++ keyboards/cantor/keymaps/kanitkameh/keymap.c
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NUMBERS] = LAYOUT_split_3x6_3(
         KC_F12,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                               KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         KC_EQL,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS,
-        _______, _______,  _______,  _______,  _______,  _______,                             _______, _______, _______, _______, _______, KC_BSLS,
+        _______,  _______,  _______,  _______,  KC_ESC,  KC_EQL,                               KC_MINUS, KC_QUOT, KC_BSLS, KC_LBRC, KC_RBRC, _______,
                                                  _______, _______, _______,          _______, _______, _______
     ),
     [LEFT_MODS] = LAYOUT_split_3x6_3(
