6fbf4612e9ad5de18abb3fef8948e0bff160b0a1
Kamen Vakavchiev <kanitkameh@gmail.com>
2023-07-31 14:17:56 +0300

    add key lock

--- keyboards/cantor/keymaps/kanitkameh/rules.mk
+++ keyboards/cantor/keymaps/kanitkameh/rules.mk
@@ -2,3 +2,4 @@ SWAP_HANDS_ENABLE = yes      # Enable one-hand typing
 LEADER_ENABLE = yes
 CAPS_WORD_ENABLE = yes   # CAPS lock but switch - for _ and automatically stop CAPS LOCK
 MOUSEKEY_ENABLE = yes
+KEY_LOCK_ENABLE = yes
